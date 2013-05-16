1988b199301535a55dab90b9d6ffcc50f1110987
Martin Thoma <info@martin-thoma.de>
2013-05-16 21:34:35 +0200

    misc

---
+++ collatz/steps-collatz-sequence.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def f(n):
+    if n % 2 == 0:
+        return n / 2
+    else:
+        return 3*n + 1
+
+if __name__ == "__main__":
+    import argparse
+ 
+    parser = argparse.ArgumentParser(
+        description="get the collatz sequence for one n"
+    )
+
+    parser.add_argument("-n", 
+                      dest="n", default=20, type=int, 
+                      help="n")
+    args = parser.parse_args()
+
+    n = args.n
+    steps = 0
+    print("steps,n")
+    while n != 1:
+        print("%i,%i" % (steps, n))
+        n = f(n)
+        steps += 1
