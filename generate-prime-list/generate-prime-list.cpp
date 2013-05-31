403280d1c732c7f22bb7a1e3857b75dfc9a5390b
Martin Thoma <info@martin-thoma.de>
2013-05-31 15:22:50 +0200

    misc

--- generate-prime-list/generate-prime-list.cpp
+++ generate-prime-list/generate-prime-list.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h> // fopen
 #include <iostream> // atoi
 #include <vector>
-
+  
 using namespace std;
 
 void sieveOfEratosthenes(long long n) {
@@ -9,10 +9,12 @@ void sieveOfEratosthenes(long long n) {
     pFile = fopen("huge-prime-list.bin", "wb");
     vector<bool> primesEratosthenes (n+1, true);
 
+    long long tmp = 2;
+    fwrite(&tmp, sizeof(long long),1, pFile);
     for (long long i=3; i<n; i+=2) {
         if (primesEratosthenes[i]) {
             fwrite(&i, sizeof(long long), 1, pFile);
-     
+
             for (long long j=i*i; j<=n; j+=i) {
                 primesEratosthenes[j] = false;
             }
@@ -23,7 +25,11 @@ void sieveOfEratosthenes(long long n) {
 }
 
 int main(int argc, char* argv[]) {
-    long long  n = (long long) atoi(argv[1]);
-    sieveOfEratosthenes(n);
+    if (argc != 2) {
+        cout << "You have to specify n" << endl;
+    } else {
+        long long  n = (long long) atoi(argv[1]);
+        sieveOfEratosthenes(n);
+    }
     return 0;
 }
--- generate-prime-list/primeList2txt.cpp
+++ generate-prime-list/primeList2txt.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    FILE* pFile;
-    pFile = fopen(argv[1], "rb");
+    if (argc != 2) {
+        cout << "You have to specify a file name" << endl;
+    } else {
+        FILE* pFile;
+        pFile = fopen(argv[1], "rb");
 
-    long long x;
-    size_t read;
-    while (!feof(pFile)) {
-        read = fread(&x, sizeof(long long), 1, pFile);
-        (void) read;
-        cout << x << endl;
+        long long x;
+        size_t read;
+        while (!feof(pFile)) {
+            read = fread(&x, sizeof(long long), 1, pFile);
+            (void) read;
+            if (feof(pFile)){
+                break; // otherwise it duplicates the last entry
+            }
+            cout << x << endl;
+        }
+        fclose(pFile);
     }
-    fclose(pFile);
 }
