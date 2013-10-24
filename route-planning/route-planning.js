0a0296f2214eb397d63047edf4d896943819932b
Martin Thoma <info@martin-thoma.de>
2013-10-24 18:27:41 +0200

    route planning added

--- route-planning/route-planning.js
+++ route-planning/route-planning.js
@@ -53,7 +53,7 @@ function getColor(i, k, transparency) {
 /** draw all permanently added points */
 function drawPolygons(canvas) {
     for(var cluster = 0; cluster < points.length; cluster++) {
-        context.fillStyle = getColor(cluster, document.getElementById("polygonNumber").max, 0.9);
+        context.fillStyle = getColor(cluster, parseInt(document.getElementById("polygonNumber").max)+4, 0.9);
         for (var i=0; i < points[cluster].length; i++) {
             context.beginPath();
             context.arc(points[cluster][i]["x"], 
@@ -88,10 +88,25 @@ function drawBoard(canvas, mouseCoords, radius) {
     context.canvas.height = window.innerHeight - 120;
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Voronoi
+    if (document.getElementById("voronoi").checked) {
+        var add = parseInt(document.getElementById("voronoiAdd").value);
+        for (var x=0; x < canvas.width; x+=add) {
+            for (var y=0; y < canvas.height; y+=add) {
+                var i = getNearestBorder(x, y);
+                //console.log(i);
+                context.fillStyle = getColor(i,points.length+4, 0.9);
+                context.fillRect(x, y, add/2, add/2);
+            }
+        }
+    }
+
     drawPolygons(canvas);
 
     // Algorithmus
-    polygonzerlegung(canvas);
+    if (document.getElementById("polygonSegmentation").checked) {
+        polygonzerlegung(canvas);
+    }
 
     // Draw Start
     var imageObj = new Image();
@@ -108,13 +123,88 @@ function drawBoard(canvas, mouseCoords, radius) {
     imageObjT.src = target["src"];
 }
 
+function getEuclideanDistance(a, b) {
+    return Math.sqrt((a["x"]-b["x"])*(a["x"]-b["x"])+(a["y"]-b["y"])*(a["y"]-b["y"]));
+}
+
+function getDistToLine(line, p) {
+    var px = line[1]["x"]-line[0]["x"];
+    var py = line[1]["y"]-line[0]["y"];
+
+    var tmp = px*px + py*py
+
+    var u =  ((p["x"] - line[0]["x"]) * px + (p["y"] - line[0]["y"]) * py) / tmp;
+
+    if (u > 1) {
+        u = 1;
+    } else if(u < 0) {
+        u = 0;
+    }
+
+    var x = line[0]["x"] + u * px;
+    var y = line[0]["y"] + u * py;
+
+    var dx = x - p["x"];
+    var dy = y - p["y"];
+
+    var dist = Math.sqrt(dx*dx + dy*dy);
+    return dist;
+}
+
+function getDistToPolygon(polygon, p) {
+    var minDist = Math.max(canvas.width, canvas.height);
+    for(var i=0; i+1<polygon.length;i++) {
+        var tmpDist = getDistToLine([polygon[i], polygon[i+1]], p);
+        if (tmpDist < minDist) {
+            minDist = tmpDist;
+        }
+    }
+    
+    var tmpDist = getDistToLine([polygon[0], polygon[polygon.length-1]], p);
+    if (tmpDist < minDist) {
+        minDist = tmpDist;
+    }
+    
+    return minDist;
+}
+
+function getNearestBorder(x, y) {
+    var d = [];
+
+    for(var cluster = 0; cluster < document.getElementById("polygonNumber").max; cluster++) {
+        d.push(getDistToPolygon(points[cluster], {"x":x, "y":y}));
+    }
+
+    // distance to "down border"
+    d.push(canvas.height - y);
+
+    // distance to "up border"
+    d.push(y);
+
+    // distance to "left border"
+    d.push(x);
+
+    // distance to "right border"
+    d.push(canvas.width - x);
+
+    var i = 0, m = d[0], mI = 0;
+    while(++i < d.length) {
+        if(d[i] < m) {
+            mI = i;
+            m = d[i];
+        }
+    }
+
+    return mI;
+}
+
 function polygonzerlegung(canvas) {
     var context = canvas.getContext('2d');
     for(var cluster = 0; cluster < points.length; cluster++) {
         context.fillStyle = getColor(0, 1, 0.9);
         for (var i=0; i < points[cluster].length; i++) {
             context.beginPath();
-            context.moveTo(0, points[cluster][i]["y"]);
+            context.moveTo(points[cluster][i]["x"], 0);
             context.lineTo(points[cluster][i]["x"], points[cluster][i]["y"]);
             context.stroke();
         }
