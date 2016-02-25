f3f0ec84fb7ec879c1a3d59102a69c1bd0d87a05
Martin Thoma <info@martin-thoma.de>
2016-02-25 13:13:44 +0100

    Add logging / interpolation experiment

--- noise/red/wrong/python.py
+++ noise/red/wrong/python.py
@@ -7,16 +7,6 @@ import numpy
 import Image
 import random
 
-import logging
-import sys
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-                    level=logging.DEBUG,
-                    stream=sys.stdout)
-
-# from sklearn.svm import SVR as Regressor
-from scipy.interpolate import griddata
-import numpy as np
-
 
 def create_red_noise(outfile, width, height, r=10):
     """
@@ -31,42 +21,6 @@ def create_red_noise(outfile, width, height, r=10):
         Random maximum offset compared to the last pixel
     """
     array = numpy.random.rand(height, width, 3) * 255
-
-    # for color_channel in range(3):
-    #     logging.info("Color channel: %i", color_channel)
-
-    #     # Generate data
-    #     x_train, y_train, x_test = [(0, 0), (width-1, height-1)], [0, 0], []
-
-    #     logging.info("Generate data...")
-    #     for nr in range(int(height*width*0.001)):
-    #         first = True
-    #         pos = None
-    #         while first or pos in x_train:
-    #             first = False
-    #             pos = (random.randint(0, width-1),
-    #                    random.randint(0, height-1))
-    #         x_train.append(pos)
-    #         y_train.append(random.randint(0, 255))
-    #     for y in range(height):
-    #         for x in range(width):
-    #             x_test.append((x, y))
-
-    #     # Fit gaussian
-    #     # gp = Regressor()
-    #     # logging.info("Fit regressor...")
-    #     # gp.fit(x_train, y_train)
-    #     y_pred = griddata(x_train, y_train,
-    #                       x_test,
-    #                       method='cubic')
-    #     # print(grid_z0)
-
-    #     # # Predict rest
-    #     # y_pred = gp.predict(x_test)
-    #     for data_nr, pos in enumerate(x_test):
-    #         x, y = pos
-    #         array[y][x][color_channel] = y_pred[data_nr]
-
     for x in range(width):
         for y in range(height):
             if y == 0:
