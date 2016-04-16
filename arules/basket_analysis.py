d2bf593d9c729c669e7eee5fa6d0118f666c570c
Martin Thoma <info@martin-thoma.de>
2016-04-16 13:37:28 +0200

    Apriori prune step added

--- arules/basket_analysis.py
+++ arules/basket_analysis.py
@@ -29,7 +29,7 @@ def get_frequent_items(itemsets, threshold=0.5):
                 frequent_items[item] += 1
             else:
                 frequent_items[item] = 1
-    frequency = [{'item': set([el[0]]), 'count': el[1]}
+    frequency = [{'itemset': set([el[0]]), 'count': el[1]}
                  for el in frequent_items.items()
                  if float(el[1])/len(itemsets) > threshold]
     return sorted(frequency, reverse=True, key=lambda n: n['count'])
@@ -83,42 +83,68 @@ def get_confidence(itemsets, itemset_a, itemset_b):
             float(get_support(itemset_a)))
 
 
-def create_k_frequent_itemsets(itemsets, items, f_items, threshold=0.5):
+def apriori(itemsets, threshold=0.05):
+    """The apriori algorithm for finding frequent itemsets."""
+    # 17.11.2015
+    assert threshold >= 0.0
+
+    frequent_itemsets = {}
+
+    # Create 1-Element frequent itemsets
+    f_items = get_frequent_items(itemsets, threshold=threshold)
+    frequent_itemsets[1] = f_items
+
+    # Create k-element frequent itemsets
+    k = 2
+    while len(f_items) > 0:
+        f_items = apriori_join(f_items)
+        f_items = apriori_prune(itemsets, f_items, threshold=threshold)
+        # Support Counting
+        frequent_itemsets[k] = f_items
+        k += 1
+        break  # TODO
+    return frequent_itemsets
+
+
+def apriori_join(f_items):
     """
-    Generate a k-frequent itemset from a (k-1) frequent itemset f_items.
+    Create (k+1)-itemsets from all frequent k-itemsets.
+
+    Those are candidates for frequent (k+1) itemsets. This step is known as
+    'candidate generation'.
+    """
+    new_f_items = []
+    for i, itemset1 in enumerate(f_items):
+        for itemset2 in f_items[i+1:]:
+            # Check if those sets, which are guaranteed to have the same
+            # number of elements, differ only by 1 element.
+            if len(itemset1['itemset'] - itemset2['itemset']) == 1:
+                new_f_items.append({'itemset': (itemset1['itemset']
+                                                .union(itemset2['itemset']))})
+    return new_f_items
+
+
+def apriori_prune(itemsets, f_items, threshold):
+    """
+    Prune all elements which don't have the required support.
 
     Parameters
     ----------
     itemsets : list
         All available itemsets.
-    items : list
-        A list of all possible items
-    f_items : list of dicts
-        A (k-1) frequent itemset.
+    f_items : list of dicts with key 'itemset'
+        Frequent k-itemsets.
     threshold : float
     """
     assert threshold >= 0.0
-    # TODO
-
-
-def apriori(itemsets, threshold=0.5):
-    """The apriori algorithm for finding frequent itemsets."""
-    # 17.11.2015
-    assert threshold >= 0.0
-
-    # Create 1-Element frequent itemsets
-    f_items = get_frequent_items(itemsets, threshold=threshold)
-
-    # Create k-element frequent itemsets
-    k = 2
-    while True:
-        # Join
-        # f_items = apriori_join(f_items)  TODO
-        # Prune
-        # Support Counting
-        f_items = create_k_frequent_itemsets(f_items, itemsets, threshold)
-        k += 1
-        break  # TODO
+    pruned_itemset = []
+    total_count = len(itemsets)
+    for item in f_items:
+        support = get_support(itemsets, item['itemset'], probability=False)
+        if support >= threshold*total_count:
+            pruned_itemset.append({'itemset': item['itemset'],
+                                   'count': support})
+    return sorted(pruned_itemset, key=lambda n: n['count'], reverse=True)
 
 
 def get_data(csv_file_path):
@@ -143,6 +169,7 @@ def get_parser():
     parser.add_argument("-f", "--file",
                         dest="filename",
                         required=True,
+                        default="groceries.csv",
                         help="CSV-FILE to read",
                         metavar="CSV-FILE")
     return parser
@@ -160,8 +187,31 @@ def main(filename):
     topline = "Top %i most frequent items" % n
     print(topline)
     print("-"*len(topline))
+    max_itemlength = max([len(", ".join(item['itemset']))
+                         for item in frequent_items[:n]])
     for item in frequent_items[:n]:
-        print("%ix %s" % (item['count'], item['item']))
+        print(("{item_count:>%i}x {item:<%i} (Support: {support:>7.4f}%%)"
+               % (len(str(frequent_items[0]['count'])), max_itemlength))
+              .format(item_count=item['count'],
+                      item=", ".join(item['itemset']),
+                      support=get_support(data,
+                                          set(item['itemset']),
+                                          probability=True)*100))
+    print("\napriori")
+    print("--------")
+    f_itemsets = apriori(data, threshold=0.01)
+    k = 1
+    while k in f_itemsets:
+        print("\nk={k}".format(k=k))
+        max_itemlength = max([len(", ".join(item['itemset']))
+                             for item in f_itemsets[k]])
+        for item in f_itemsets[k]:
+            print(("{item_count:>%i}x {item:<%i} (Support: {support:>7.4f}%%)"
+                   % (len(str(f_itemsets[k][0]['count'])), max_itemlength))
+                  .format(item_count=item['count'],
+                          item=", ".join(item['itemset']),
+                          support=(float(item['count'])/len(data))*100))
+        k += 1
 
 if __name__ == '__main__':
     args = get_parser().parse_args()
