8e0cfa9960f6a34a38a0f7b4dae37923304e0330
Martin Thoma <info@martin-thoma.de>
2017-01-26 15:45:26 +0100

    HASY: Add t-sne

--- ML/hasy/t-sne-vis.py
+++ ML/hasy/t-sne-vis.py
@@ -3,14 +3,18 @@
 """Visualize with tsne as in https://indico.io/blog/visualizing-with-t-sne/."""
 
 import numpy as np
-from skdata.mnist.views import OfficialImageClassification
+import hasy_tools as ht
 from matplotlib import pyplot as plt
 from tsne import bh_sne
 
 # load up data
-data = OfficialImageClassification(x_dtype="float32")
-x_data = data.all_images
-y_data = data.all_labels
+dataset_path = './HASYv2'
+data_complete = []
+
+symbol_id2index = ht.generate_index("%s/symbols.csv" % dataset_path)
+x_data, y_data = ht.load_images('HASYv2/hasy-data-labels.csv',
+                                symbol_id2index,
+                                one_hot=False)
 
 # convert image data to float64 matrix. float64 is need for bh_sne
 x_data = np.asarray(x_data).astype('float64')
@@ -28,7 +32,8 @@ vis_data = bh_sne(x_data)
 vis_x = vis_data[:, 0]
 vis_y = vis_data[:, 1]
 
-plt.scatter(vis_x, vis_y, c=y_data, cmap=plt.cm.get_cmap("jet", 10))
-plt.colorbar(ticks=range(10))
-plt.clim(-0.5, 9.5)
-plt.show()
+plt.scatter(vis_x, vis_y, c=y_data, cmap=plt.cm.get_cmap("jet", 369))
+plt.colorbar(ticks=range(369))
+# plt.clim(-0.5, 9.5)
+filename = '{}.pdf'.format('tsne')
+plt.savefig(filename)
