f9d2f1543ccd68982c082b6bd794895d08ba2e1c
Martin Thoma <info@martin-thoma.de>
2017-02-20 22:50:18 +0100

    ML/mnist/tf: Clean example up

--- ML/mnist/tensorflow-example/mnist_softmax.py
+++ ML/mnist/tensorflow-example/mnist_softmax.py
@@ -1,35 +1,64 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""A very simple MNIST classifier.
-
-See extensive documentation at
-http://tensorflow.org/tutorials/mnist/beginners/index.md
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+#!/usr/bin/env python
+"""A very simple MNIST classifier."""
 import argparse
 import sys
 
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
+ce_with_logits = tf.nn.softmax_cross_entropy_with_logits
 
 FLAGS = None
+
+
+def inference(x):
+    """
+    Build the inference graph.
+
+    Parameters
+    ----------
+    x : placeholder
+
+    Returns
+    -------
+    Output tensor with the computed logits.
+    """
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.matmul(x, W) + b
+    return y
+
+
+def loss(logits, labels):
+    """
+    Calculate the loss from the logits and the labels.
+
+    Parameters
+    ----------
+    logits : Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels : Labels tensor, int32 - [batch_size]
+    """
+    cross_entropy = tf.reduce_mean(ce_with_logits(labels=labels,
+                                                  logits=logits))
+    return cross_entropy
+
+
+def training(loss, learning_rate=0.5):
+    """
+    Set up the training Ops.
+
+    Parameters
+    ----------
+    loss : Loss tensor, from loss().
+    learning_rate : The learning rate to use for gradient descent.
+
+    Returns
+    -------
+    train_op: The Op for training.
+    """
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_step = optimizer.minimize(loss)
+    return train_step
 
 
 def main(_):
@@ -38,29 +67,16 @@ def main(_):
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, 784])
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
-    y = tf.matmul(x, W) + b
+    y = inference(x)
 
     # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
-
-    # The raw formulation of cross-entropy,
-    #
-    #     tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-    #                                                                 reduction_indices=[1]))
-    #
-    # can be numerically unstable.
-    #
-    # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-    # outputs of 'y', and then average across the batch.
-    cross_entropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    loss_ = loss(logits=y, labels=y_)
+    train_step = training(loss_)
 
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
-    # Train
+
     for _ in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
