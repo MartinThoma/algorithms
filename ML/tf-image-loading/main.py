946bbda8a98dcaa7aa2e2dcc6ff5da21cc9a2cf6
Martin Thoma <info@martin-thoma.de>
2017-02-23 10:25:27 +0100

    ML/tf-image-loading: Use tf.train.Example

--- ML/tf-image-loading/main.py
+++ ML/tf-image-loading/main.py
@@ -24,6 +24,18 @@ with tf.Session() as sess:
     for i in range(len(filenames)):
         image = my_img.eval()  # image is an image tensor
 
+    # Create tf.train.Example
+    bytes_list = tf.train.BytesList(value=image.tobytes())
+    feat = tf.train.Feature(bytes_list=bytes_list)
+    ex = tf.train.Example(features=tf.train.Features(feature={'image': feat}))
+
+    # Write tf.train.Example
+    writer = tf.python_io.TFRecordWriter("example.tfrecords")
+    writer.write(ex.SerializeToString())
+
+    # Read tf.train.Example
+    # TODO
+
     print(image.shape)
     im = Image.fromarray(np.asarray(image))
     im.show()
