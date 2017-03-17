04aeeef056f936883be8a4ff8d5ba3b477409372
Martin Thoma <info@martin-thoma.de>
2017-03-17 11:01:41 +0100

    ML/cifar/rl: Add Batch Normalization; cifar10 instead of cifar100

--- ML/cifar/rl/rflearn.py
+++ ML/cifar/rl/rflearn.py
@@ -8,11 +8,12 @@ np.random.seed(0)
 from keras.models import Model
 from keras.layers import Input, merge, Dense, Flatten
 from keras.layers.convolutional import Convolution2D
+from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
 import keras.backend as K
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.datasets import cifar100
+from keras.datasets import cifar10
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 import csv
@@ -44,60 +45,73 @@ def create_rflearn_net(nb_classes, img_dim):
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(x1)
     m3 = merge([x1, x2], mode='concat', concat_axis=concat_axis)
+    m3 = BatchNormalization(axis=concat_axis)(m3)
     x3 = Convolution2D(36, 3, 3,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m3)
     m4 = merge([x1, x2, x3], mode='concat', concat_axis=concat_axis)
+    m4 = BatchNormalization(axis=concat_axis)(m4)
     x4 = Convolution2D(36, 5, 5,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m4)
     m5 = merge([x3, x4], mode='concat', concat_axis=concat_axis)
+    m5 = BatchNormalization(axis=concat_axis)(m5)
     x5 = Convolution2D(48, 7, 3,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m5)
     m6 = merge([x2, x3, x4, x5], mode='concat', concat_axis=concat_axis)
+    m6 = BatchNormalization(axis=concat_axis)(m6)
     x6 = Convolution2D(48, 7, 7,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m6)
     m7 = merge([x2, x3, x4, x5, x6], mode='concat', concat_axis=concat_axis)
+    m7 = BatchNormalization(axis=concat_axis)(m7)
     x7 = Convolution2D(48, 7, 7,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m7)
     m8 = merge([x1, x6, x7], mode='concat', concat_axis=concat_axis)
+    m8 = BatchNormalization(axis=concat_axis)(m8)
     x8 = Convolution2D(36, 3, 7,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m8)
     m9 = merge([x1, x5, x6, x8], mode='concat', concat_axis=concat_axis)
+    m9 = BatchNormalization(axis=concat_axis)(m9)
     x9 = Convolution2D(36, 1, 7,
                        init="he_uniform", border_mode="same", bias=True,
                        W_regularizer=l2(1E-4))(m9)
     m10 = merge([x1, x3, x4, x5, x6, x7, x8, x9],
                 mode='concat', concat_axis=concat_axis)
+    m10 = BatchNormalization(axis=concat_axis)(m10)
     x10 = Convolution2D(36, 7, 7,
                         init="he_uniform", border_mode="same", bias=True,
                         W_regularizer=l2(1E-4))(m10)
     m11 = merge([x1, x2, x5, x6, x7, x8, x9, x10],
                 mode='concat', concat_axis=concat_axis)
+    m11 = BatchNormalization(axis=concat_axis)(m11)
     x11 = Convolution2D(36, 7, 5,
                         init="he_uniform", border_mode="same", bias=True,
                         W_regularizer=l2(1E-4))(m11)
     m12 = merge([x1, x2, x3, x4, x6, x11],
                 mode='concat', concat_axis=concat_axis)
+    m12 = BatchNormalization(axis=concat_axis)(m12)
     x12 = Convolution2D(36, 7, 5,
                         init="he_uniform", border_mode="same", bias=True,
                         W_regularizer=l2(1E-4))(m12)
     m13 = merge([x1, x3, x6, x7, x8, x9, x10, x11, x12],
                 mode='concat', concat_axis=concat_axis)
+    m13 = BatchNormalization(axis=concat_axis)(m13)
     x13 = Convolution2D(48, 5, 7,
                         init="he_uniform", border_mode="same", bias=True,
                         W_regularizer=l2(1E-4))(m13)
     m14 = merge([x3, x7, x12, x13],
                 mode='concat', concat_axis=concat_axis)
+    m14 = BatchNormalization(axis=concat_axis)(m14)
     x14 = Convolution2D(48, 5, 7,
                         init="he_uniform", border_mode="same", bias=True,
                         W_regularizer=l2(1E-4))(m14)
     m15 = merge([x2, x7, x11, x12, x13, x14],
                 mode='concat', concat_axis=concat_axis)
+    m15 = BatchNormalization(axis=concat_axis)(m15)
     x15 = Convolution2D(48, 5, 7,
                         init="he_uniform", border_mode="same", bias=True,
                         W_regularizer=l2(1E-4))(m15)
@@ -108,7 +122,7 @@ def create_rflearn_net(nb_classes, img_dim):
 
 
 if __name__ == '__main__':
-    nb_classes = 100
+    nb_classes = 10
     img_rows, img_cols, img_channels = 32, 32, 3
     batch_size = 32
     nb_epoch = 100
@@ -120,13 +134,13 @@ if __name__ == '__main__':
         img_dim = (img_rows, img_cols, img_channels)
     model = create_rflearn_net(nb_classes=nb_classes, img_dim=img_dim)
     model.summary()
-    optimizer = Adam(lr=1e-4)
+    optimizer = Adam(lr=1e-3)
     model.compile(loss='categorical_crossentropy',
                   optimizer=optimizer,
                   metrics=["accuracy"])
 
     # Load data
-    (X_train, y_train), (X_test, y_test) = cifar100.load_data()
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
     X_train /= 255.0
