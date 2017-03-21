06ffd3364d508aa49d8a462ab17bdffb0ca3b590
Martin Thoma <info@martin-thoma.de>
2017-03-21 09:46:22 +0100

    Python: Add generator example

---
+++ Python/generator/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+def square_counter(is_true):
+    if is_true:
+        i = 0
+        while True:
+            yield i**2
+            i += 1
+            if i > 5:
+                return
+    else:
+        i = 5
+        while True:
+            yield i * 3
+            i += 1
+            if i > 5:
+                return
+
+g = square_counter(False)
+for el in g:
+    print(el)
