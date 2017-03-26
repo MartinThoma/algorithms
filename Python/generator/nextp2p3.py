798e385c3e1f7e45d0c945380cd85c91a63d407e
Martin Thoma <info@martin-thoma.de>
2017-03-26 22:01:19 +0200

    Python/generator/nextp2p3.py

---
+++ Python/generator/nextp2p3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+def square_gen(is_true):
+    i = 0
+    while True:
+        yield i**2
+        i += 1
+
+g = square_gen(False)
+print(next(g))
+print(next(g))
+print(next(g))
