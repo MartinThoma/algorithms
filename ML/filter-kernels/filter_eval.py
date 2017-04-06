3fe7708aaab5002a64eba1ea7ca20e4fe7668a72
Martin Thoma <info@martin-thoma.de>
2017-04-06 12:13:45 +0200

    Filter eval: name magic numbers

--- ML/filter-kernels/filter_eval.py
+++ ML/filter-kernels/filter_eval.py
@@ -34,12 +34,14 @@ def make_mosaic(imgs, nrows, ncols, border=1):
 im = imread('EmiMa-099.jpg', mode='L')
 imshow(im)
 imgs = []
-for i in range(9):
-    weights = numpy.random.random((3, 3))
+nrows = 5
+ncols = 5
+for i in range(nrows * ncols):
+    weights = numpy.random.random((3, 3))  # Filter size
     print(weights)
     im_res = convolve2d(im, weights, mode='same')
     imgs.append(im_res)
 imgs = numpy.array(imgs)
-mosaic = make_mosaic(imgs, nrows=3, ncols=3, border=1)
+mosaic = make_mosaic(imgs, nrows=nrows, ncols=ncols, border=1)
 print(mosaic.shape)
 imshow(mosaic)
