9b553b4a207fa35755e2ff8cc3172a8031e8219e
Martin Thoma <info@martin-thoma.de>
2017-06-15 08:41:39 +0200

    ML/mnist: Add keras and tflearn

---
+++ ML/mnist/mnist.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Utility file for the MNIST dataset."""
+
+from keras.utils.data_utils import get_file
+import numpy as np
+import os
+import scipy.misc
+from sklearn.model_selection import train_test_split
+
+n_classes = 10
+labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+WIDTH = 32  # 28
+HEIGHT = 32  # 28
+img_rows = 32  # 28
+img_cols = 32  # 28
+img_channels = 1
+
+_mean_filename = "mnist-mean.npy"
+
+
+def load_data(config):
+    """
+    Load the MNIST dataset.
+
+    # Arguments
+        path: path where to cache the dataset locally
+            (relative to ~/.keras/datasets).
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    path = 'mnist.npz'
+    path = get_file(path,
+                    origin='https://s3.amazonaws.com/img-datasets/mnist.npz')
+    f = np.load(path)
+    x_train = f['x_train']
+    y_train = f['y_train']
+    x_test = f['x_test']
+    y_test = f['y_test']
+    f.close()
+
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,
+                                                      test_size=0.10,
+                                                      random_state=42,
+                                                      stratify=y_train)
+
+    return {'x_train': x_train, 'y_train': y_train,
+            'x_val': x_val, 'y_val': y_val,
+            'x_test': x_test, 'y_test': y_test}
+
+
+def preprocess(x, subtact_mean=False):
+    """Preprocess features."""
+    x = x.astype('float32')
+    x_new = np.zeros((len(x), 32, 32), dtype=np.uint8)
+    for i, x_i in enumerate(x):
+        x_new[i] = scipy.misc.imresize(x_i, (32, 32))
+    x = x_new
+    x = x.astype('float32')
+    if not subtact_mean:
+        x /= 255.0
+    else:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        mean_path = os.path.join(dir_path, _mean_filename)
+        mean_image = np.load(mean_path)
+        x -= mean_image
+        x /= 128.
+    x = x.reshape(x.shape[0], x.shape[1], x.shape[2], 1)
+    return x
+
+
+if __name__ == '__main__':
+    config = {'dataset': {}}
+    data = load_data(config)
+    print("Training data n={}".format(len(data['x_train'])))
+    print("Validation data n={}".format(len(data['x_val'])))
+    print("Test data n={}".format(len(data['x_test'])))
+    mean_image = np.mean(data['x_train'], axis=0)
+    np.save(_mean_filename, mean_image)
+    import scipy.misc
+    scipy.misc.imshow(mean_image)
+    for img, label in zip(data['x_train'], data['y_train']):
+        print(globals()['labels'][label])
+        scipy.misc.imshow(img)
---
+++ ML/mnist/tflearn_mnist.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""Train a CNN model on MNIST."""
+
+from __future__ import print_function
+import tflearn
+from keras.utils import np_utils
+import mnist
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+
+# training specific hyperparameters
+batch_size = 128
+epochs = 1
+
+# Load the data, shuffled and split between train and test sets
+data = mnist.load_data({'dataset': {}})
+x_train = data['x_train']
+y_train = data['y_train']
+x_test = data['x_test']
+y_test = data['y_test']
+
+# Bring data into necessary format
+x_train = mnist.preprocess(x_train, subtact_mean=False)
+x_test = mnist.preprocess(x_test, subtact_mean=False)
+y_train = np_utils.to_categorical(y_train, mnist.n_classes)
+y_test = np_utils.to_categorical(y_test, mnist.n_classes)
+
+# Define model
+input_shape = (mnist.img_rows, mnist.img_cols, 1)
+model = input_data(shape=input_shape, name='input')
+model = conv_2d(model, 32, 3, activation='relu', regularizer="L2")
+model = conv_2d(model, 64, 3, activation='relu', regularizer="L2")
+model = max_pool_2d(model, 2)
+model = dropout(model, 0.25)
+model = fully_connected(model, 128, activation='relu')
+model = dropout(model, 0.5)
+model = fully_connected(model, mnist.n_classes, activation='softmax')
+model = regression(model, optimizer='adam', learning_rate=0.01,
+                   loss='categorical_crossentropy', name='target')
+
+# Fit model
+model = tflearn.DNN(model, tensorboard_verbose=0)
+model.fit({'input': x_train}, {'target': y_train}, n_epoch=epochs,
+          validation_set=({'input': x_test}, {'target': y_test}),
+          snapshot_step=len(x_train), show_metric=True, run_id='convnet_mnist')
+
+# Evaluate model
+score = model.evaluate(x_test, y_test)
+print('Test accuracy: {:0.2f}%'.format(score[0] * 100))
