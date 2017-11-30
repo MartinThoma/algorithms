5d2cb294f3a7ebb1e60d740046c7b07f682c7b39
Martin Thoma <info@martin-thoma.de>
2017-11-30 20:48:58 +0100

    ML/RL: Add DQN agent

--- ML/rl/dqn_agent.py
+++ ML/rl/dqn_agent.py
@@ -1,106 +1,195 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import numpy as np
-import gym
-import gym_banana
+"""
+Deep Q-Learning agent.
 
+This was initially taken from
+https://github.com/keon/deep-q-learning/blob/master/dqn.py and
+refactored quite a bit.
+"""
+
+# core modules
+from collections import deque
+import logging
+import os
+import random
+import sys
+
+# 3rd party modules
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
+import gym
+import numpy as np
 
-from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
-from rl.memory import EpisodeParameterMemory
+# local modules
+from rl_utils import get_parser, load_cfg, test_agent
 
+np.random.seed(280490)
 
-def main(env_name, nb_steps):
-    # Get the environment and extract the number of actions.
-    env = gym.make(env_name)
-    np.random.seed(123)
-    env.seed(123)
-
-    nb_actions = env.action_space.n
-    input_shape = (1,) + env.observation_space.shape
-    model = create_nn_model(input_shape, nb_actions)
-
-    # Finally, we configure and compile our agent.
-    memory = EpisodeParameterMemory(limit=10000, window_length=1)
-
-    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1.,
-                                  value_min=.1, value_test=.05,
-                                  nb_steps=1000000)
-    agent = DQNAgent(model=model, nb_actions=nb_actions, policy=policy,
-                     memory=memory, nb_steps_warmup=1000000,
-                     gamma=.99, target_model_update=1000,
-                     train_interval=4, delta_clip=1.)
-    agent.compile(Adam(lr=.00025), metrics=['mae'])
-    agent.fit(env, nb_steps=nb_steps, visualize=False, verbose=1)
-
-    # After training is done, we save the best weights.
-    agent.save_weights('dqn_{}_params.h5f'.format(env_name), overwrite=True)
-
-    # Finally, evaluate the agent
-    history = agent.test(env, nb_episodes=100, visualize=False)
-    rewards = np.array(history.history['episode_reward'])
-    print(("Test rewards (#episodes={}): mean={:>5.2f}, std={:>5.2f}, "
-           "min={:>5.2f}, max={:>5.2f}")
-          .format(len(rewards),
-                  rewards.mean(),
-                  rewards.std(),
-                  rewards.min(),
-                  rewards.max()))
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG,
+                    stream=sys.stdout)
+np.set_printoptions(formatter={'float_kind': lambda x: "{:>5.2f}".format(x)})
 
 
-def create_nn_model(input_shape, nb_actions):
+def main(environment_name, agent_cfg_file):
     """
-    Create a neural network model which maps the input to actions.
+    Load, train and evaluate a Reinforcment Learning agent.
 
     Parameters
     ----------
-    input_shape : tuple of int
-    nb_actoins : int
-
-    Returns
-    -------
-    model : keras Model object
+    environment_name : str
+    agent_cfg_file : str
     """
-    model = Sequential()
-    if len(input_shape) == 3:
-        model.add(Flatten(input_shape=input_shape))
-        model.add(Dense(32))
-    else:
-        model.add(Dense(32, input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dense(nb_actions))
-    model.add(Activation('linear'))
-    print(model.summary())
-    return model
+    cfg = load_cfg(agent_cfg_file)
+
+    # Set up environment and agent
+    env = gym.make(environment_name)
+    cfg['env'] = env
+    cfg['serialize_path'] = ('artifacts/{}-{}.pickle'
+                             .format(cfg['model_name'], environment_name))
+    agent = load_agent(cfg, env)
+
+    agent = train_agent(cfg, env, agent)
+    rewards = test_agent(cfg, env, agent)
+    print("Average reward: {:5.3f}".format(rewards))
+    print("Trained episodes: {}".format(agent.episode))
 
 
-def get_parser():
-    """Get parser object for script xy.py."""
-    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--env",
-                        dest="environment",
-                        help="OpenAI Gym environment",
-                        metavar="ENVIRONMENT",
-                        default="CartPole-v0")
-    parser.add_argument("--steps",
-                        dest="steps",
-                        default=10000,
-                        type=int,
-                        help="how steps are trained?")
-    return parser
+class DQNAgent:
+    """
+    DQN Agent.
+
+    Parameters
+    ----------
+    cfg : dict
+    """
+
+    def __init__(self, cfg, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=cfg['memory']['length'])
+        self.gamma = cfg['problem']['gamma']    # discount rate
+        self.epsilon = cfg['training']['exploration']['epsilon']
+        self.epsilon_min = cfg['training']['exploration']['epsilon_min']
+        self.epsilon_decay = cfg['training']['exploration']['epsilon_decay']
+        self.learning_rate = cfg['training']['learning_rate']
+        self.model = self._build_model()
+
+        self.episode = 0
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        state = self._preprocess_state(state)
+        next_state = self._preprocess_state(state)
+        self.memory.append((state, action, reward, next_state, done))
+
+    def reset(self):
+        """Reset the agent. Call this at the beginning of an episode."""
+        self.episode += 1
+
+    def act(self, state, no_exploration=False):
+        state = self._preprocess_state(state)
+        if not no_exploration and np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def _preprocess_state(self, state):
+        state = np.reshape(state, [1, self.state_size])
+        return state
+
+    def save(self, name):
+        """Serialize an agent."""
+        self.model.save_weights(name)
+        return self
+
+    def load(self, name):
+        """Load an agent."""
+        self.model.load_weights(name)
+        return self
+
+
+def load_agent(cfg, env):
+    """
+    Create (and load) a QTableAgent.
+
+    Parameters
+    ----------
+    cfg : dict
+    env : OpenAI environment
+    """
+    agent = DQNAgent(cfg, env.observation_space.shape[0], env.action_space.n)
+    if os.path.isfile(cfg['serialize_path']):
+        agent.load(cfg['serialize_path'])
+    return agent
+
+
+# General training and testing code
+def train_agent(cfg, env, agent):
+    """
+    Train an agent in environment.
+
+    Parameters
+    ----------
+    cfg : dict
+    env : OpenAI environment
+    agent : object
+
+    Return
+    ------
+    agent : object
+    """
+    rewards = []
+    window_size = cfg['training']['print_window_size']
+    max_episode_length = cfg['training']['max_episode_length']
+    for episode in range(cfg['training']['nb_episodes']):
+        agent.reset()
+        observation_previous = env.reset()
+        is_done = False
+        time = 0
+        while not (time >= max_episode_length) and not is_done:
+            action = agent.act(observation_previous)
+            observation, reward, is_done, _ = env.step(action)
+            rewards.append(reward)
+            agent.remember(observation_previous, action, reward, observation,
+                           is_done)
+            observation_previous = observation
+            time += 1
+        if episode % cfg['training']['print_score'] == 0 and episode > 0:
+            agent.save(cfg['serialize_path'])
+            print("Average score: {:>5.2f}"
+                  .format(sum(rewards[-window_size:]) / window_size))
+        if len(agent.memory) > cfg['training']['batch_size']:
+            agent.replay(cfg['training']['batch_size'])
+    agent.save(cfg['serialize_path'])
+    return agent
 
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
-    main(args.environment, args.steps)
+    main(args.environment_name, args.agent_cfg_file)
