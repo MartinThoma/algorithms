edcdbc01b61edaebe300c6ff3966df0930cfa31e
Martin Thoma <info@martin-thoma.de>
2018-11-19 18:07:26 +0100

    ENH: Add ML/50-mlps/13-keras-cnn-dropout

---
+++ ML/50-mlps/13-keras-cnn-dropout/create_validation_curve.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Visualize validation curve."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Y_SCALE_FACTOR = 100
+
+# Prepare dataframe
+df = pd.read_csv('log.csv', sep=';')
+df = df[['epoch', 'acc', 'val_acc']]
+df[['acc', 'val_acc']] = df[['acc', 'val_acc']] * Y_SCALE_FACTOR
+df = df.set_index('epoch').rename(columns={'acc': 'Training Accuracy',
+                                           'val_acc': 'Validation Accuracy'})
+print(df)
+
+# Plot
+fig, ax = plt.subplots()
+df.plot.line(ylim=(0.75 * Y_SCALE_FACTOR, 1.00 * Y_SCALE_FACTOR),
+             title='Validation Curve',
+             ax=ax)
+ax.minorticks_on()  # required for minor grid
+ax.grid()
+ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.savefig('validation-curve.png', dpi=300)
