95199d130106f05b159094e0d80e1d096eeb850d
Martin Thoma <info@martin-thoma.de>
2018-11-29 18:09:37 +0100

    Add model 27

---
+++ ML/50-mlps/27-keras-mlp/create_validation_curve.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Visualize validation curve."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Y_SCALE_FACTOR = 100
+
+# Prepare dataframe
+df = pd.read_csv('log.csv', sep=';')
+if 'val_acc' not in df:
+    df['val_acc'] = 0.8
+df = df[['epoch', 'acc', 'val_acc']]
+df[['acc', 'val_acc']] = df[['acc', 'val_acc']] * Y_SCALE_FACTOR
+df = df.set_index('epoch').rename(columns={'acc': 'Training Accuracy',
+                                           'val_acc': 'Validation Accuracy'})
+print(df)
+
+# Plot
+fig, ax = plt.subplots()
+df.plot.line(ylim=(0.50 * Y_SCALE_FACTOR, 1.00 * Y_SCALE_FACTOR),
+             title='Validation Curve',
+             ax=ax)
+ax.minorticks_on()  # required for minor grid
+ax.grid()
+ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.savefig('validation-curve.png', dpi=300)
---
+++ ML/50-mlps/27-keras-mlp/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 3rd party modules
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras.layers import Dense, Flatten, Activation, Dropout
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+# internal modules
+import hasy_tools
+
+# Load the data
+data = hasy_tools.load_data()
+datasets = ['train', 'test']
+
+# One-Hot encoding
+for dataset in datasets:
+    key = 'y_' + dataset
+    data[key] = np.eye(hasy_tools.n_classes)[data[key].squeeze()]
+
+# Preprocessing
+for dataset in datasets:
+    key = 'x_' + dataset
+    data[key] = hasy_tools.preprocess(data[key])
+
+# Generate Validation Data
+split = train_test_split(data['x_train'], data['y_train'],
+                         test_size=0.20,
+                         random_state=0,
+                         stratify=data['y_train'])
+data['x_train'], data['x_val'], data['y_train'], data['y_val'] = split
+datasets.append('val')
+
+# Define the model
+model = Sequential()
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dense(256))
+model.add(Dropout(0.50))
+model.add(Activation('relu'))
+model.add(Dense(hasy_tools.n_classes, activation='softmax'))
+
+# Compile model
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# Fit the model
+csv_logger = CSVLogger('log.csv', append=True, separator=';')
+checkpointer = ModelCheckpoint(filepath='checkpoint.h5',
+                               verbose=1,
+                               period=10,
+                               save_best_only=True)
+model.fit(data['x_train'], data['y_train'],
+          validation_data=(data['x_val'], data['y_val']),
+          epochs=500,
+          batch_size=128,
+          callbacks=[csv_logger, checkpointer])
+
+# Serialize model
+model.save('model.h5')
+
+# evaluate the model
+scores = model.evaluate(data['x_test'], data['y_test'])
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
