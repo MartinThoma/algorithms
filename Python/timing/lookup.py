c8a9416ba8ab9a6339039e13d281369e74a77773
Martin Thoma <martin.thoma.extern@sixt.com>
2019-04-18 18:37:59 +0200

    ENH: Add get_webpage

--- Python/timing/lookup.py
+++ Python/timing/lookup.py
@@ -14,23 +14,34 @@ def main():
     d = dict((i, [random.randint(1, n) for j in range(100)])
              for i in range(n))
     list_ = [value for _, value in sorted(d.items())]
+    list_int = [random.randint(0, 25) for _ in range(1000)]
+    list_float = [random.random() for _ in range(1000)]
     array_ = np.array(list_)
     d_numbers = [random.randint(0, n - 1) for i in range(1000)]
     functions = [(add, 'add', nr_list),
-                 (vector_add, 'add_vectors', vectors),
                  (subtract, 'subtract', nr_list),
-                 (vector_subtract, 'subtract_vectors', vectors),
                  (multiply, 'multiply', nr_list),
-                 (vector_multiply, 'multiply_vectors', vectors),
                  (divide, 'divide', nr_list),
+                 (vector_add, 'add_vectors', vectors),
+                 (vector_subtract, 'subtract_vectors', vectors),
+                 (vector_multiply, 'multiply_vectors', vectors),
                  (vector_divide, 'divide_vectors', vectors),
+                 (power, '2**int (0..20)', (2, list_int)),
+                 (power, '3**int (0..20)', (3, list_int)),
+                 (power, '3.131**int (0..20)', (3.141, list_int)),
+                 (power, '2**float', (2, list_float)),
+                 (power, '3**float', (3, list_float)),
+                 (power, '3.131**float', (3.141, list_float)),
                  (lookup, 'lookup(dict)', (d, d_numbers)),
                  (lookup, 'lookup(list)', (list_, d_numbers)),
                  (lookup, 'lookup(np array)', (array_, d_numbers)),
+                 (get_webpage, 'get_webpage (google.de)', 'http://google.de'),
+                 (get_webpage, 'get_webpage (martin-thoma.de)', 'http://martin-thoma.de'),
+                 (get_webpage, 'get_webpage (stackoverflow.com)', 'https://stackoverflow.com/'),
                  ]
     duration_list = {}
     for func, name, params in functions:
-        durations = timeit.repeat(lambda: func(params), repeat=5000, number=3)
+        durations = timeit.repeat(lambda: func(params), repeat=10, number=3)
         duration_list[name] = list(np.array(durations) * 1000)
         print('{func:<20}: '
               'min: {min:5.1f}μs, mean: {mean:5.1f}μs, max: {max:6.1f}μs'
@@ -98,9 +109,26 @@ def vector_divide(params):
     return v1 / v2
 
 
+def power(params):
+    base, numbers = params
+    return [base**number for number in numbers]
+
+
 def lookup(params):
     d, numbers = params
     return [d[index] for index in numbers]
+
+
+def get_webpage(url):
+    try:
+        # For Python 3.0 and later
+        from urllib.request import urlopen
+    except ImportError:
+        # Fall back to Python 2's urllib2
+        from urllib2 import urlopen
+    response = urlopen(url)
+    data = str(response.read())
+    return data
 
 
 if __name__ == '__main__':
