3bfb5b0b014048e17ec5fee11924c8bbafd89c72
Martin Thoma <info@martin-thoma.de>
2019-10-05 08:10:03 +0200

    Database benchmark: Add more tests

--- Python/databases/benchmark/benchmark.py
+++ Python/databases/benchmark/benchmark.py
@@ -29,12 +29,26 @@ memcached
 ---------
 $ sudo apt-get install libmemcached-tools
 $ memcdump --servers=localhost | wc -l
+# Clear all cashes
+$ echo 'flush_all' | nc localhost 11211
+
+
 
 Redis
 -----
+$ redis-cli
+
+Delete what is in Redis:
+> FLUSHALL
+> FLUSHDB
+
+Show what is in Redis:
+> KEYS '*'
+
 """
 
 # core modules
+import gc
 import json
 import random
 import time
@@ -49,6 +63,7 @@ import click
 import numpy as np
 import pandas as pd
 import sqlalchemy
+import redis
 
 
 Base = declarative_base()
@@ -73,7 +88,9 @@ class KeyValue(Base):
         return f"KeyValue(key='{self.key}', value='{self.value}')"
 
 
-def run_benchmark(SQLALCHEMY_DATABASE_URI, n=1000, benchmark_type="orm"):
+def run_benchmark(
+    SQLALCHEMY_DATABASE_URI, n=1000, batch_size=None, benchmark_type="orm"
+):
     if SQLALCHEMY_DATABASE_URI is not None:
         engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
         connection = engine.connect()
@@ -93,21 +110,39 @@ def run_benchmark(SQLALCHEMY_DATABASE_URI, n=1000, benchmark_type="orm"):
     if benchmark_type == "orm":
         benchmark_orm_insert(session, keys, values)
     elif benchmark_type == "orm-bulk":
-        benchmark_orm_bulk_insert(session, keys, values)
+        truncate_query = """DELETE FROM "KeyValue";"""
+        if "mysql" in SQLALCHEMY_DATABASE_URI.lower():
+            truncate_query = """DELETE FROM KeyValue;"""
+        session.execute(truncate_query)
+        benchmark_orm_bulk_insert(session, keys, values, batch_size)
+        gc.collect()
+        benchmark_orm_bulk_select(session)
+        gc.collect()
+        select_keys = np.random.choice(keys, 10 ** 6)
+        print("Generated selection keys.")
+        benchmark_random_selection(session, select_keys)
     elif benchmark_type == "raw":
         benchmark_raw_insert(connection, keys, values)
     elif benchmark_type == "print-inserts":
         print_extended_inserts(keys, values)
     elif benchmark_type == "datafile":
-        print_datafile(keys, values)
+        generate_datafile(keys, values)
     elif benchmark_type == "redis":
-        benchmark_redis_insert(keys, values)
+        benchmark_redis_insert(keys, values, batch_size=batch_size)
+        benchmark_redis_bulk_select()
+        select_keys = np.random.choice(keys, 10 ** 6)
+        print("Generated selection keys.")
+        benchmark_redis_select(select_keys)
     elif benchmark_type == "pickledb":
         benchmark_pickledb_insert(keys, values)
     elif benchmark_type == "memcached":
         benchmark_memcached_insert(keys, values)
+        select_keys = np.random.choice(keys, 10 ** 6)
+        benchmark_memcached_select(select_keys)
     elif benchmark_type == "dict":
-        benchmark_dict_insert(keys, values)
+        store = benchmark_dict_insert(keys, values)
+        select_keys = np.random.choice(keys, 10 ** 2)
+        benchmark_dict_select(store, select_keys)
     else:
         raise NotImplementedError(
             f"benchmark_type={benchmark_type} is not implemented."
@@ -130,6 +165,24 @@ def benchmark_dict_insert(keys, values):
         f"dict: Inserted {len(keys)} entries in {t1 - t0:0.2f}s "
         f"({len(keys)/(t1 - t0):0.2f} inserts/s)."
     )
+    return store
+
+
+def benchmark_dict_select(store, keys):
+    latencies = []
+    for key in keys:
+        t0 = time.time()
+        key_value = store[key]
+        t1 = time.time()
+        latencies.append(t1 - t0)
+    latencies = np.array(latencies) * 10 ** 6
+    print(
+        f"Selected {len(keys)} entries in {sum(latencies):0.2f}s "
+        f"({len(keys)/sum(latencies):0.2f} selects/s, min={min(latencies):0.0f}μs, "
+        f"25%={np.percentile(latencies, 25):0.0f}μs, 50%={np.percentile(latencies, 50):0.0f}μs, "
+        f"95%={np.percentile(latencies, 95):0.0f}μs, "
+        f"99%={np.percentile(latencies, 99):0.0f}μs, 100%={np.percentile(latencies, 100):0.0f}μs)."
+    )
 
 
 def benchmark_orm_insert(session, keys, values):
@@ -145,19 +198,53 @@ def benchmark_orm_insert(session, keys, values):
     )
 
 
-def benchmark_orm_bulk_insert(session, keys, values):
+def benchmark_orm_bulk_insert(session, keys, values, batch_size):
     t0 = time.time()
-    objects = [KeyValue(key=key, value=value) for key, value in zip(keys, values)]
-    session.bulk_save_objects(objects)
+    batch = []
+    for key, value in zip(keys, values):
+        batch.append(KeyValue(key=key, value=value))
+        if len(batch) == batch_size:
+            session.bulk_save_objects(batch)
+            batch = []
+            session.commit()
+    if len(batch) > 0:
+        session.bulk_save_objects(batch)
     session.commit()
     t1 = time.time()
     print(
         f"Inserted {len(keys)} entries in {t1 - t0:0.2f}s with ORM-Bulk "
-        f"({len(keys)/(t1 - t0):0.2f} inserts/s)."
+        f"({len(keys)/(t1 - t0):0.2f} inserts/s, batch_size={batch_size})."
     )
 
 
-def benchmark_redis_insert(keys, values):
+def benchmark_orm_bulk_select(session):
+    t0 = time.time()
+    key_values = session.query(KeyValue).all()
+    t1 = time.time()
+    print(
+        f"Selected {len(key_values)} entries in {t1 - t0:0.2f}s "
+        f"({len(key_values)/(t1 - t0):0.2f} selects/s)."
+    )
+
+
+def benchmark_random_selection(session, keys):
+    latencies = []
+    for key in keys:
+        t0 = time.time()
+        key_value = session.query(KeyValue).filter(KeyValue.key == key)
+        t1 = time.time()
+        latencies.append(t1 - t0)
+    latencies = np.array(latencies) * 10 ** 6
+    print(
+        f"Random selected performance for {len(keys)} entries: "
+        f"min={min(latencies):0.0f}μs, "
+        f"25%={np.percentile(latencies, 25):0.0f}μs, 50%={np.percentile(latencies, 50):0.0f}μs, "
+        f"95%={np.percentile(latencies, 95):0.0f}μs, "
+        f"99%={np.percentile(latencies, 99):0.0f}μs, 100%={np.percentile(latencies, 100):0.0f}μs)."
+    )
+
+
+def benchmark_redis_insert(keys, values, batch_size):
     """
 
     Notes
@@ -178,14 +265,26 @@ def benchmark_redis_insert(keys, values):
     Redis: Inserted 10000 entries in 0.22s (45966.79 inserts/s).
     ```
     """
-    import redis
-
     r = redis.Redis(host="localhost")
+
     t0 = time.time()
-    pipe = r.pipeline()
-    for key, value in zip(keys, values):
-        pipe.set(key, value)
-    pipe.execute()
+    if batch_size is None:
+        pipe = r.pipeline()
+        for key, value in zip(keys, values):
+            pipe.set(key, value)
+        pipe.execute()
+    else:
+        pipe = r.pipeline()
+        i = 0
+        for key, value in zip(keys, values):
+            pipe.set(key, value)
+            i += 1
+            if i == batch_size:
+                i = 0
+                pipe.execute()
+                pipe = r.pipeline()
+        if i > 0:
+            pipe.execute()
     t1 = time.time()
     print(
         f"Redis: Inserted {len(keys)} entries in {t1 - t0:0.2f}s "
@@ -193,17 +292,38 @@ def benchmark_redis_insert(keys, values):
     )
 
 
+def benchmark_redis_select(keys):
+    r = redis.Redis(host="localhost")
+    latencies = []
+    for key in keys:
+        t0 = time.time()
+        key_value = r.get(key)
+        t1 = time.time()
+        latencies.append(t1 - t0)
+    latencies = np.array(latencies) * 10 ** 6
+    print(
+        f"Redis: Random get performance for {len(keys)}:"
+        f"min={min(latencies):0.0f}us, "
+        f"25%={np.percentile(latencies, 25):0.0f}us, 50%={np.percentile(latencies, 50):0.0f}us, "
+        f"95%={np.percentile(latencies, 95):0.0f}us, "
+        f"99%={np.percentile(latencies, 99):0.0f}us, 100%={np.percentile(latencies, 100):0.0f}us)."
+    )
+
+
+def benchmark_redis_bulk_select():
+    r = redis.Redis(host="localhost")
+    t0 = time.time()
+    keys = r.keys()
+    key_values = r.mget(keys)
+    t1 = time.time()
+    print(
+        f"Redis: Selected {len(key_values)} entries in {t1 - t0:0.2f}s "
+        f"({len(key_values)/(t1 - t0):0.2f} selects/s)."
+    )
+
+
 def benchmark_memcached_insert(keys, values):
     """
-
-    Notes
-    -----
-    Show what is in Redis:
-    > KEYS '*'
-
-    Delete what is in Redis:
-    > FLUSHDB
-
     Benchmark Results
     -----------------
     ```
@@ -225,6 +345,27 @@ def benchmark_memcached_insert(keys, values):
     print(
         f"memcached: Inserted {len(keys)} entries in {t1 - t0:0.2f}s "
         f"({len(keys)/(t1 - t0):0.2f} inserts/s)."
+    )
+
+
+def benchmark_memcached_select(keys):
+    from pymemcache.client.base import Client
+
+    client = Client(("localhost", 11211))
+
+    latencies = []
+    for key in keys:
+        t0 = time.time()
+        key_value = client.get(key)
+        t1 = time.time()
+        latencies.append(t1 - t0)
+    latencies = np.array(latencies) * 10 ** 6
+    print(
+        f"Memcached: Random get performance for {len(keys)} entries: "
+        f"min={min(latencies):0.0f}us, "
+        f"25%={np.percentile(latencies, 25):0.0f}us, 50%={np.percentile(latencies, 50):0.0f}us, "
+        f"95%={np.percentile(latencies, 95):0.0f}us, "
+        f"99%={np.percentile(latencies, 99):0.0f}us, 100%={np.percentile(latencies, 100):0.0f}us)."
     )
 
 
@@ -265,7 +406,7 @@ def print_extended_inserts(keys, values):
     print(";")
 
 
-def print_datafile(keys, values):
+def generate_datafile(keys, values):
     df = pd.DataFrame(data={"key": keys, "value": values})
     df.to_csv("data.csv", index=False, quotechar="'")
 
@@ -273,6 +414,7 @@ def print_datafile(keys, values):
 @click.command()
 @click.option("--db", "db", required=True, type=click.Choice(list(db_dict.keys())))
 @click.option("-n", "n", required=True, type=int)
+@click.option("--batch-size", "batch_size", required=False, default=None, type=int)
 @click.option(
     "--mode",
     "mode",
@@ -291,8 +433,8 @@ def print_datafile(keys, values):
         ]
     ),
 )
-def entry_point(db, n, mode):
-    run_benchmark(db_dict[db], n, mode)
+def entry_point(db, n, batch_size, mode):
+    run_benchmark(db_dict[db], n, batch_size, mode)
 
 
 if __name__ == "__main__":
