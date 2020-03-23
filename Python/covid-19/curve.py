af04ac3e4c6268cd5a7b1dfa120278c7cd60a864
Martin Thoma <info@martin-thoma.de>
2020-03-23 17:52:17 +0100

    ENH: Add corona scripts

---
+++ Python/covid-19/curve.py
@@ -0,0 +1,115 @@
+# Core Library modules
+import datetime
+import math
+from typing import List
+
+# Third party modules
+import matplotlib
+import matplotlib.pyplot as plt
+
+# First party modules
+from regression import LogitRegressor, get_data
+
+matplotlib.use("TKAgg", warn=False, force=True)
+
+
+simulation_day_start = 0
+switching_day = 100
+simulation_day_end = 360
+critical_care_percentage = 0.05
+intensive_care_beds = 28_000
+
+
+model = LogitRegressor(beta=0.1980, c=13.4600, d=1.0000, max_population=20_000_000.0)
+model_flat = LogitRegressor(
+    beta=0.05, c=13.4600, d=1.0000, max_population=20_000_000.0, shift_x=120
+)
+
+
+def get_new_infections(model, simulation_day_start, simulation_day_end) -> List[float]:
+    new_infected_list = []
+    for day in range(simulation_day_start, simulation_day_end):
+        if day == 0:
+            new_infected_list.append(0)
+        else:
+            new_infected = model.predict([day])[0] - model.predict([day - 1])[0]
+            new_infected_list.append(new_infected)
+    return new_infected_list
+
+
+with plt.xkcd():
+    fig, ax = plt.subplots(1, 1, figsize=(15, 6))
+    y_max = model.max_population
+    total_infected = model.predict(range(simulation_day_start, simulation_day_end))
+    new_infected_list = get_new_infections(
+        model, simulation_day_start, simulation_day_end
+    )
+
+    day = datetime.datetime.strptime("2020-02-24", "%Y-%m-%d")
+    print("date, predicted accummulated sick, new sick")
+    for sick_people, new_infected in zip(total_infected, new_infected_list):
+        print(f"{day:%Y-%m-%d}: {sick_people:,.0f} (+{new_infected:,.0f})")
+        day += datetime.timedelta(seconds=24 * 60 * 60)
+    # plt.plot(
+    #     list(range(simulation_day_start, simulation_day_end)),
+    #     total_infected,
+    # )
+    plt.plot(list(range(simulation_day_start, simulation_day_end)), new_infected_list)
+
+    flat_infected = []
+    flat_infected = get_new_infections(model, simulation_day_start, switching_day)
+
+    if len(flat_infected) == 0:
+        x = 0
+    else:
+        x = model.get_x(flat_infected[-1])
+    print(f"Switch day: {x}")
+    print("-" * 80)
+    delta = x - switching_day
+    add = len(range(int(simulation_day_start + delta), int(simulation_day_end + delta)))
+    delta_list = len(new_infected_list) - add
+    flat_infected += get_new_infections(
+        model_flat,
+        int(simulation_day_start + delta) - delta_list,
+        int(simulation_day_end + delta),
+    )
+    flat_infected = get_new_infections(
+        model_flat, simulation_day_start, simulation_day_end
+    )
+    plt.plot(
+        list(range(simulation_day_start, simulation_day_start + len(flat_infected))),
+        flat_infected,
+    )
+    plt.plot(
+        list(range(simulation_day_start, simulation_day_start + len(flat_infected))),
+        [intensive_care_beds for _ in range(simulation_day_start, simulation_day_end)],
+        "--",
+    )
+    plt.plot(
+        list(range(simulation_day_start, simulation_day_start + len(flat_infected))),
+        [
+            intensive_care_beds / critical_care_percentage
+            for _ in range(simulation_day_start, simulation_day_end)
+        ],
+        "--",
+    )
+    plt.xlabel("Days after first infection")
+    plt.ylabel("New infections per day")
+    plt.text(250, intensive_care_beds + 30_000, "Intensive Care Beds", color="green")
+    plt.text(
+        250,
+        intensive_care_beds + intensive_care_beds / critical_care_percentage,
+        "Healthcare carrying capacity",
+        color="red",
+    )
+    plt.legend(["new infected", "new infected (lower growth)"])  # "sick (accumulated)",
+
+    def formatter(x, pos):
+        prefixes = [("m", 10 ** 6), ("k", 10 ** 3)]
+        for prefix, amount in prefixes:
+            if x >= amount:
+                return f"{x / amount}{prefix}"
+        return "{:,}".format(x)
+
+    plt.gca().yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(formatter))
+    plt.show()
---
+++ Python/covid-19/regression.py
@@ -0,0 +1,137 @@
+# Core Library modules
+import csv
+import datetime
+import math
+from typing import List, Tuple
+
+# Third party modules
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+
+
+def get_data(filepath: str) -> Tuple[List[int], List[float]]:
+    with open(filepath, "r") as fp:
+        reader = csv.reader(fp, delimiter=",", quotechar='"')
+        next(reader, None)  # skip the headers
+        data = [row for row in reader]
+
+    xs, ys = zip(*data)
+    dates = [datetime.datetime.strptime(x, "%Y-%m-%d") for x in xs]
+    xs = [(x - dates[0]).days for x in dates]
+    ys = [float(y) for y in ys]
+    return dates, xs, ys
+
+
+class LogitRegressor(BaseEstimator, ClassifierMixin):
+    """
+    Fit data to the model:
+
+    y = 1 / (d + e^(-beta * x + c))
+    """
+
+    def __init__(
+        self,
+        beta: float = 1,
+        c: float = 0,
+        d: float = 1,
+        max_population=1,
+        shift_x: float = 0,
+    ):
+        self.beta = beta
+        self.c = c
+        self.d = d
+        self.max_population = max_population
+        self.shift_x = shift_x
+
+    def get_x(self, y: float) -> float:
+        left, right = 0, 1000
+        left_value = self.predict([left])
+        right_value = self.predict([right])
+
+        assert left_value[0] < y
+        assert right_value[0] > y
+        while right - left > 0.1:
+            middle = (right + left) / 2
+            middle_value = self.predict([middle])[0]
+            if middle_value < y:
+                left = middle
+                left_value = middle_value
+            else:
+                right = middle
+                right_value = middle_value
+        return middle
+
+    def fit(self, X, y):
+        xs = X
+
+        # Make a grid-search to find the best values
+        min_mse = float("inf")
+        best_prams = {"beta": 1, "c": 0, "d": 1.0}
+        for beta_tmp in np.arange(start=0.15, stop=0.25, step=0.001):
+            for c_tmp in np.arange(start=12, stop=14, step=0.001):
+                for d_tmp in np.arange(start=1, stop=2, step=1):
+                    self.beta = beta_tmp
+                    self.c = c_tmp
+                    self.d = d_tmp
+                    mse = mean_squared_error(self.predict(X), y)
+                    if mse < min_mse:
+                        min_mse = mse
+                        best_prams["beta"] = beta_tmp
+                        best_prams["c"] = c_tmp
+                        best_prams["d"] = d_tmp
+
+        # Set to the best value
+        self.beta = best_prams["beta"]
+        self.c = best_prams["c"]
+        self.d = best_prams["d"]
+        return self
+
+    def predict(self, X: List[float]) -> List[float]:
+        # y = 1 / (d + e^(-beta * x + c))
+        return [
+            self.max_population
+            / (self.d + math.exp(-self.beta * (x + self.shift_x) + self.c))
+            for x in X
+        ]
+
+    def __str__(self):
+        return (
+            f"LogitRegressor(beta={self.beta:0.4f}, c={self.c:0.4f}, "
+            f"d={self.d:0.4f}, "
+            f"max_population={self.max_population})"
+        )
+
+    __repr__ = __str__
+
+
+def find_model_for_germany():
+    dates, xs, ys = get_data("infections_germany.csv")
+    max_population = 80_000_000 * 0.8
+
+    model = LogitRegressor(max_population=max_population).fit(X=xs, y=ys)
+
+    # Print the found model
+    print(model)
+
+    # Print some output to see goodness of fit
+    infected_pred_list = []
+    for day, x_pred, x_correct in zip(dates, model.predict(xs), ys):
+        daily_infected_pred = x_pred
+        daily_infected_true = x_correct
+        infected_pred_list.append(daily_infected_pred)
+        if len(infected_pred_list) >= 2:
+            new_infected = infected_pred_list[-1] - infected_pred_list[-2]
+        else:
+            new_infected = daily_infected_pred
+        print(
+            f"Day {day:%Y-%m-%d}: {daily_infected_pred:,.0f} (+{new_infected:,.0f}) predicted vs {daily_infected_true:,.0f} in reality"
+        )
+
+    mse = mean_squared_error(model.predict(xs), ys)
+    print(f"MSE = {mse:,.5f}")
+
+
+if __name__ == "__main__":
+    find_model_for_germany()
