8c4f9954c612533d91140c1f813fc648120f4d5f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 03:34:55 +0000

    Adding tarfile member sanitization to extractall()

--- PyPI/package_analysis.py
+++ PyPI/package_analysis.py
@@ -198,7 +198,26 @@ def download(package_url: str) -> Tuple[List[str], Optional[str]]:
         if is_tarfile:
             try:
                 with tarfile.open(target) as tar:
-                    tar.extractall(target[:-file_ending_len])
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, target[:-file_ending_len])
             except Exception:
                 print(f"Something is wrong with the tar file of {pkg_name}")
                 # Something is wrong with the tar file
